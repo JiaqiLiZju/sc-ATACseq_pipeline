8479b0b5dc6f8aefc461b0dde75ba0190f6b98de
JiaqiLiZju <46318417+JiaqiLiZju@users.noreply.github.com>
2021-05-16 07:40:27 +0800

    QC

--- CH-snapATAC/deeptools.sh
+++ CH-snapATAC/deeptools.sh
@@ -10,15 +10,14 @@ bamCoverage -p 5 --normalizeUsing RPKM -b human.sorted.bam -o human.sorted.bw &>
 # bamCoverage --normalizeUsing CPM -b $id -o ${id%%.*}.rm.bw & 
 
 ## both -R and -S can accept multiple files 
-computeMatrix reference-point \
+computeMatrix reference-point -p 15 \
     --referencePoint TSS \
-    -b 10000 -a 10000 \
+    -b 1000 -a 1000 \
     -R ./Mix.Human.Refseq.bed \
     -S ./human.sorted.bw \
     --skipZeros \
     -o matrix1_test_TSS.gz \
     --outFileSortedRegions regions1_test_genes.bed
-
 ## both plotHeatmap and plotProfile will use the output from computeMatrix
 plotHeatmap -m matrix1_test_TSS.gz  -out test_Heatmap.png
 plotHeatmap -m matrix1_test_TSS.gz  -out test_Heatmap.pdf --plotFileFormat pdf  --dpi 720  
@@ -26,10 +25,10 @@ plotProfile -m matrix1_test_TSS.gz  -out test_Profile.png
 plotProfile -m matrix1_test_TSS.gz  -out test_Profile.pdf --plotFileFormat pdf --perGroup --dpi 720 
 
 ## gene body
-computeMatrix scale-regions  -p 15  \
-    -R ~/project/atac/mm10_Refgene/Refseq.bed  \
-    -S ~/project/atac/deeptools_result/*.bw  \
-    -b 10000 -a 10000  \
+computeMatrix scale-regions -p 15 \
+    -R ./Mix.Human.Refseq.bed \
+    -S ./human.sorted.bw \
+    -b 1000 -a 1000  \
     --skipZeros -o matrix1_test_body.gz
 # plotHeatmap -m matrix1_test_body.gz  -out ExampleHeatmap1.png
 plotHeatmap -m matrix1_test_body.gz  -out test_body_Heatmap.png
---
+++ Reference/mix.Refseq.sh
@@ -0,0 +1,1 @@
+cat hg38.Refseq.bed |sed "s/chr/HUMAN_/g" >Mix.Human.Refseq.bed
